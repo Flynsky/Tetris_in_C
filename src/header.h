0f646bb885a98fdfb9aeebf9612b124ffbaaa407
Fynn Gewiese <gewiese@hm.edu>
2024-06-27 12:38:42 +0200

    faster field refresh

--- src/header.h
+++ src/header.h
@@ -1,9 +1,20 @@
 #ifndef HEADER_H
 #include "precompiled.h"
+#define FIELD_METHOD char *field, const u_int lenght_x, const u_int lenght_y // makes method defines more readable
+#define FIELD_CALL field, LENGHT_X, LENGHT_Y                                 // makes method calls more readable
 
-void draw_field(const char *playfield, const u_int lenght_x, const u_int lenght_y);
-void fill_field(char *playfield, const u_int lenght_x, const u_int lenght_y,const char character);
+// ANSI escape sequences
+#define ANSI_CLEAR_SCREEN "\033[2J"
+#define ANSI_HOME_CURSOR "\033[H"
+
+void gemeloop();
+void setup();
+
+void draw_field(const FIELD_METHOD);
+void clear_terminal();
+void fill_field_with_char(FIELD_METHOD, const char character);
+void draw_point(FIELD_METHOD, const u_int Xpos, const u_int Ypos, char block);
 
 unsigned long long millis();
-void print_fps();
+unsigned int measure_fps();
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,113 @@
 
 /** look for out\compile_and_run.bat*/
 
-const u_int LENGHT_X = 2;
-const u_int LENGHT_Y = 2;
-char field[] = {LENGHT_X * LENGHT_Y};
+const u_int LENGHT_X = 20;
+const u_int LENGHT_Y = 20;
+u_int tick_time = 200; // time between each game tick in ms
+
+char field[LENGHT_X * LENGHT_Y] = {0};
 
 int main()
 {
-    print_fps();
-    fill_field(field, LENGHT_X, LENGHT_Y, 'O');
-    draw_field(field, LENGHT_X, LENGHT_Y);
+    setup();
+    gemeloop();
     return 0;
 }
 
-void fill_field(char *playfield, const u_int lenght_x, const u_int lenght_y, const char character)
+void gemeloop()
+{
+    static unsigned long long last_tick = millis();
+    for (;;)
+    {
+        if (millis() > last_tick)
+        {
+            last_tick = millis() + tick_time;
+            static u_int a = 0;
+
+            fill_field_with_char(FIELD_CALL, 'O');
+
+            draw_point(FIELD_CALL, 1, a, 'X');
+            a += 1;
+            draw_field(FIELD_CALL);
+        }
+        fill_field_with_char(FIELD_CALL, '_');
+        draw_point(FIELD_CALL, 1, 1, 'X');
+        draw_field(FIELD_CALL);
+    }
+}
+
+void setup()
+{
+    u_int fps = measure_fps();
+    printf("\nfps of console: %u\n", fps);
+}
+
+/**
+ *draws a single char int the field
+ */
+void draw_point(FIELD_METHOD, const u_int Xpos, const u_int Ypos, char block)
+{
+    field[Xpos + LENGHT_X * Ypos] = block;
+}
+
+/**
+ * places a block
+ */
+void draw_block(FIELD_METHOD, const u_int Xpos, const u_int Ypos, const u_int Type)
+{
+    switch (Type)
+    {
+    case 0:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
+}
+
+void fill_field_with_char(FIELD_METHOD, const char character)
 {
     for (u_int i = 0; i < (lenght_y * lenght_x); i++)
     {
-        playfield[i] = character;
+        field[i] = character;
     }
 }
 
-void draw_field(const char *playfield, const u_int lenght_x, const u_int lenght_y)
+/**outputs the field to the console */
+void draw_field(const FIELD_METHOD)
 {
-    for (u_int y = 0; y < lenght_y; y++)
+    const int lenght_row = lenght_x * 2 + 2; // with start and end simbol
+    const int size_buffer = sizeof(char) * lenght_row * lenght_y - 1;
+
+    char *consoleBuffer = (char *)malloc(size_buffer);
+
+    for (int y = 0; y < lenght_y; y += 1)
     {
-        printf("|");
-        for (u_int x = 0; x < lenght_x; x++)
+        consoleBuffer[y * lenght_row] = '|';
+        for (int x = 0; x < lenght_x; x += 1)
         {
-            printf(" %c |", playfield[x + y * lenght_x]);
+            consoleBuffer[y * lenght_row + x * 2 + 1] = field[x + y * lenght_y];
+            consoleBuffer[y * lenght_row + x * 2 + 2] = '|';
         }
-        printf("\n");
+        consoleBuffer[y * lenght_row - 1] = '\n';
     }
+    clear_terminal();
+    fwrite(consoleBuffer, sizeof(char), size_buffer, stdout);
+    fflush(stdout);
 }
 
-/**prints the refreshrate of the console */
-void print_fps()
+void clear_terminal()
+{
+    system("cls");
+    // printf(ANSI_CLEAR_SCREEN);
+    // printf(ANSI_HOME_CURSOR);
+    // for(int i = 0;i<(LENGHT_Y+2);i++){
+    // printf("\n");}
+}
+
+/**returns the refreshrate of the console */
+unsigned int measure_fps()
 {
     const unsigned long duration_ms = 100;
     const unsigned long long start_time = millis();
@@ -44,16 +116,15 @@ void print_fps()
     printf("\n");
     for (;;)
     {
-        printf("time till launch: %ums counter: %u \r", millis() - start_time, a);
+        printf("time till launch: %llums counter: %u \r", millis() - start_time, a);
         a += 1;
 
         if (millis() - start_time > duration_ms)
         {
-            printf("FPS console: %i                        \n", a / ((millis() - start_time) / duration_ms));
-            break;
+            return (u_int)(a / ((millis() - start_time) / duration_ms));
         }
     }
-    printf("\n");
+    printf("                                                   ");
 }
 
 // returns time since pc start in ms
