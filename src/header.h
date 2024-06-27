ee0dfb5fd8b7e544f654e0365933429d785fe486
Fynn Gewiese <gewiese@hm.edu>
2024-06-27 14:38:53 +0200

    before change of Macro to strcut for main playingfield

--- src/header.h
+++ src/header.h
@@ -1,7 +1,6 @@
 #ifndef HEADER_H
 #include "precompiled.h"
-#define FIELD_METHOD char *field, const u_int lenght_x, const u_int lenght_y // makes method defines more readable
-#define FIELD_CALL field, LENGHT_X, LENGHT_Y                                 // makes method calls more readable
+#include ".\rendering\render.h"
 
 // ANSI escape sequences
 #define ANSI_CLEAR_SCREEN "\033[2J"
@@ -10,11 +9,28 @@
 void gemeloop();
 void setup();
 
+unsigned long long millis();
+unsigned int measure_fps();
+
+const unsigned int LENGHT_X = 20;
+const unsigned int  LENGHT_Y = 20;
+
+#define FIELD_METHOD char *field, const unsigned int lenght_x, const unsigned int lenght_y // makes method defines more readable
+#define FIELD_CALL field, LENGHT_X, LENGHT_Y    // makes method calls more readable
+
+struct field{
+    char buffer[LENGHT_X*LENGHT_Y]={'0'};
+    char length_x = LENGHT_X;
+    char length_y = LENGHT_Y;
+}_field;
+
+                             
+
+
 void draw_field(const FIELD_METHOD);
 void clear_terminal();
 void fill_field_with_char(FIELD_METHOD, const char character);
-void draw_point(FIELD_METHOD, const u_int Xpos, const u_int Ypos, char block);
+void draw_point(FIELD_METHOD, const unsigned int Xpos, const unsigned int Ypos, char block);
 
-unsigned long long millis();
-unsigned int measure_fps();
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "header.h"
+#include ".\rendering\render.h"
 
 /** look for out\compile_and_run.bat*/
 
-const u_int LENGHT_X = 20;
-const u_int LENGHT_Y = 20;
-u_int tick_time = 200; // time between each game tick in ms
+u_int tick_time = 100; // time between each game tick in ms
 
 char field[LENGHT_X * LENGHT_Y] = {0};
 
@@ -24,16 +23,11 @@ void gemeloop()
         {
             last_tick = millis() + tick_time;
             static u_int a = 0;
-
             fill_field_with_char(FIELD_CALL, 'O');
-
             draw_point(FIELD_CALL, 1, a, 'X');
             a += 1;
             draw_field(FIELD_CALL);
         }
-        fill_field_with_char(FIELD_CALL, '_');
-        draw_point(FIELD_CALL, 1, 1, 'X');
-        draw_field(FIELD_CALL);
     }
 }
 
@@ -41,70 +35,6 @@ void setup()
 {
     u_int fps = measure_fps();
     printf("\nfps of console: %u\n", fps);
-}
-
-/**
- *draws a single char int the field
- */
-void draw_point(FIELD_METHOD, const u_int Xpos, const u_int Ypos, char block)
-{
-    field[Xpos + LENGHT_X * Ypos] = block;
-}
-
-/**
- * places a block
- */
-void draw_block(FIELD_METHOD, const u_int Xpos, const u_int Ypos, const u_int Type)
-{
-    switch (Type)
-    {
-    case 0:
-        /* code */
-        break;
-
-    default:
-        break;
-    }
-}
-
-void fill_field_with_char(FIELD_METHOD, const char character)
-{
-    for (u_int i = 0; i < (lenght_y * lenght_x); i++)
-    {
-        field[i] = character;
-    }
-}
-
-/**outputs the field to the console */
-void draw_field(const FIELD_METHOD)
-{
-    const int lenght_row = lenght_x * 2 + 2; // with start and end simbol
-    const int size_buffer = sizeof(char) * lenght_row * lenght_y - 1;
-
-    char *consoleBuffer = (char *)malloc(size_buffer);
-
-    for (int y = 0; y < lenght_y; y += 1)
-    {
-        consoleBuffer[y * lenght_row] = '|';
-        for (int x = 0; x < lenght_x; x += 1)
-        {
-            consoleBuffer[y * lenght_row + x * 2 + 1] = field[x + y * lenght_y];
-            consoleBuffer[y * lenght_row + x * 2 + 2] = '|';
-        }
-        consoleBuffer[y * lenght_row - 1] = '\n';
-    }
-    clear_terminal();
-    fwrite(consoleBuffer, sizeof(char), size_buffer, stdout);
-    fflush(stdout);
-}
-
-void clear_terminal()
-{
-    system("cls");
-    // printf(ANSI_CLEAR_SCREEN);
-    // printf(ANSI_HOME_CURSOR);
-    // for(int i = 0;i<(LENGHT_Y+2);i++){
-    // printf("\n");}
 }
 
 /**returns the refreshrate of the console */
@@ -149,4 +79,70 @@ unsigned long long millis()
     }
 
     return (counter.QuadPart * 1000ULL) / frequency.QuadPart;
+}
+
+
+
+/**
+ *draws a single char int the field
+ */
+void draw_point(FIELD_METHOD, const unsigned int Xpos, const unsigned int Ypos, char block)
+{
+    field[Xpos + LENGHT_X * Ypos] = block;
+}
+
+/**
+ * places a block
+ */
+void draw_block(FIELD_METHOD, const unsigned int Xpos, const unsigned int Ypos, const unsigned int Type)
+{
+    switch (Type)
+    {
+    case 0:
+        /* code */
+        break;
+
+    default:
+        break;
+    }
+}
+
+void fill_field_with_char(FIELD_METHOD, const char character)
+{
+    for (unsigned int i = 0; i < (lenght_y * lenght_x); i++)
+    {
+        field[i] = character;
+    }
+}
+
+/**outputs the field to the console */
+void draw_field(const FIELD_METHOD)
+{
+    const int lenght_row = lenght_x * 2 + 2; // with start and end simbol
+    const int size_buffer = sizeof(char) * lenght_row * lenght_y - 1;
+
+    char *consoleBuffer = (char *)malloc(size_buffer);
+
+    for (int y = 0; y < lenght_y; y += 1)
+    {
+        consoleBuffer[y * lenght_row] = '|';
+        for (int x = 0; x < lenght_x; x += 1)
+        {
+            consoleBuffer[y * lenght_row + x * 2 + 1] = field[x + y * lenght_y];
+            consoleBuffer[y * lenght_row + x * 2 + 2] = '|';
+        }
+        consoleBuffer[y * lenght_row - 1] = '\n';
+    }
+    clear_terminal();
+    fwrite(consoleBuffer, sizeof(char), size_buffer, stdout);
+    fflush(stdout);
+}
+
+void clear_terminal()
+{
+    system("cls");
+    // printf(ANSI_CLEAR_SCREEN);
+    // printf(ANSI_HOME_CURSOR);
+    // for(int i = 0;i<(LENGHT_Y+2);i++){
+    // printf("\n");}
 }
